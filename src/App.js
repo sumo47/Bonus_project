231c9ce4f8049603ab6d8a761fd98d9bcf45d77b
sumit kumar <sumokhan76342@gmail.com>
2023-06-01 17:53:16 +0530

    fix route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Quotes from './Components/Quotes/Quotes'
-import Counter from './Components/Counter_App/Counter'
+import Quotes from './Components/Quotes'
+import Counter from './Components/Counter'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
